5f2d91745a483fdd1bb0790a9ae2b472eedd39e1
geetika-agarwal <geetikaagarwal2021@gmail.com>
2022-12-20 17:02:33 +0530

    All basic HTML Routing and Endpoints are added

--- src/app/admin.service.ts
+++ src/app/admin.service.ts
@@ -19,10 +19,6 @@ export class AdminService {
     return this.http.get<any>(`http://localhost:8080/admin/login/?email=${data.email}&password=${data.password}`)
   }
 
-  registerConsumer(consumer: Consumer):Observable<String> {
-    return this.http.post<String>("http://localhost:8080/admin/register-consumer", consumer);
-  }
-
   viewAreas():Observable<Area[]>{
     return this.http.get<Area[]>("http://localhost:8080/admin/area");
   }
@@ -47,11 +43,47 @@ export class AdminService {
     return this.http.get<Bill[]>("http://localhost:8080/admin/view-all-bills")
   }
 
+  viewBillByCid(email: string):Observable<Bill[]> {
+    return this.http.get<Bill[]>(`http://localhost:8080/admin/view-bill-by-cid/?email=${email}`)
+  }
+
+  viewBillByMnY(month: string, year: number):Observable<Bill[]> {
+    return this.http.get<Bill[]>(`http://localhost:8080/admin/view-bill-by-mny/?month=${month}&year=${year}`)
+  }
+
+  viewBillByCity(city: string):Observable<Bill[]> {
+    return this.http.get<Bill[]>(`http://localhost:8080/admin/view-bill-by-city/?city=${city}`)
+  }
+
+  viewBillByArea(area: string):Observable<Bill[]> {
+    return this.http.get<Bill[]>(`http://localhost:8080/admin/view-bill-by-area/?area=${area}`)
+  }
+
   viewAreaByCity(city: string):Observable<Area[]> {
     return this.http.get<Area[]>(`http://localhost:8080/admin/area-by-city?city=${city}`)
   }
 
   modifyAreaName(data: any): any {
     return this.http.put<any>(`http://localhost:8080/admin/modify-area?areaName=${data.areaName}&newAreaName=${data.newAreaName}`, data);
+  }
+
+  addCity(data: any): any {
+    return this.http.get<any>(`http://localhost:8080/admin/add-city/?name=${data.city}`)
+  }
+
+  addArea(data: any): any {
+    return this.http.get<any>(`http://localhost:8080/admin/add-area/?areaname=${data.area}&cityname=${data.city}`)
+  }
+
+  modifyCityAreaName(data: any): any {
+    return this.http.get<any>(`http://localhost:8080/admin/modify-area-city?areaName=${data.areaName}&newCityName=${data.newCityName}`);
+  }
+
+  modifyCityName(data: any): any {
+    return this.http.get<any>(`http://localhost:8080/admin/modify-city?cityName=${data.cityName}&newCityName=${data.newCityName}`);
+  }
+
+  modifyRate(data: any): any {
+    return this.http.get<any>(`http://localhost:8080/admin/modify-consumer-type-rate?typeName=${data.consumerType}&rate=${data.newRate}`);
   }
 }
--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -6,6 +6,7 @@ import { Bill } from '../bill';
 import { City } from '../city';
 import { Consumer } from '../consumer';
 import { ConsumerType } from '../consumer-type';
+import { ConsumerService } from '../consumer.service';
 import { Helper } from '../helper';
 
 @Component({
@@ -21,13 +22,24 @@ export class AdminComponent {
   consumers: Consumer[] = []
   helpers: Helper[] = []
   bills: Bill[] = []
+  billsByCid: Bill[] = []
+  billsByMnY: Bill[] = []
+  billsByCity: Bill[] = []
+  billsByArea: Bill[] = []
   areasByCity: Area[] = []
+  months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
+  Area: string = ""
+  Month: string = ""
+  City: string = ""
+  consumertype: string = ""
   cityName: string = ""
   areaName: string = ""
   newAreaName: string = ""
+  registerAreaName: string =""
+  registerConsumerType: string = ""
 
-  constructor(public adminService: AdminService) {
+  constructor(public adminService: AdminService, public consumerService: ConsumerService) {
     this.adminService.viewAreas().subscribe(areas =>  {
       this.areas = areas;
     });
@@ -51,6 +63,7 @@ export class AdminComponent {
     this.adminService.viewBills().subscribe(bill => {
       this.bills = bill;
     })
+
   }
 
   /*------------------------------------Consumer Registration--------------------------------------------*/
@@ -60,7 +73,29 @@ export class AdminComponent {
   }
 
   showRegisterConsumerForm() {
+    document.querySelector("form")?.reset();
     document.getElementById("register-consumer-form")!.style.display = "block";
+  }
+
+  registerconsumer(data:any) {
+    if(data.email === null || data.password === null || data.name === null || data.areaName===null|| data.consumerTypeName===null
+      || data.email.trim() === "" || data.password.trim() === "" || data.name.trim() === "") {
+      alert("Fill all the values!!!")
+    } else {
+    console.log(data)
+    this.consumerService.registerConsumer(data).subscribe(
+      (data:any) => console.log(data),
+      (error: HttpErrorResponse) => {
+        if(error.status === 202) {
+          alert("Registered Successfully!!!")
+
+          document.querySelector("form")?.reset();
+        } else {
+          alert(error.error)
+        }
+      }
+    )
+    }
   }
 
   /*------------------------------------Add City--------------------------------------------*/
@@ -73,6 +108,26 @@ export class AdminComponent {
     document.getElementById("add-city-form")!.style.display = "none";
   }
 
+  addcity(data:any) {
+    if(data.city === null || data.city.trim() === "") {
+      alert("Fill the City Name")
+    } else {
+    console.log(data)
+    this.adminService.addCity(data).subscribe(
+      (data:any) => console.log(data),
+      (error: HttpErrorResponse) => {
+        if(error.status === 202) {
+          alert("City Added successfully!!!")
+
+          document.querySelector("form")?.reset();
+        } else {
+          alert(error.error)
+        }
+      }
+    )
+    }
+  }
+
   /*------------------------------------Add Area--------------------------------------------*/
 
   showAddAreaForm() {
@@ -81,6 +136,26 @@ export class AdminComponent {
 
    closeAddAreaForm() {
     document.getElementById("add-area-form")!.style.display = "none";
+  }
+
+  addarea(data:any) {
+    if(data.city === null || data.area.trim() === "" || data.area === null) {
+      alert("Fill the Area Details")
+    } else {
+    console.log(data)
+    this.adminService.addArea(data).subscribe(
+      (data:any) => console.log(data),
+      (error: HttpErrorResponse) => {
+        if(error.status === 202) {
+          alert("City Added successfully!!!")
+
+          document.querySelector("form")?.reset();
+        } else {
+          alert(error.error)
+        }
+      }
+    )
+    }
   }
 
   /*------------------------------------View All Consumers--------------------------------------------*/
@@ -143,6 +218,123 @@ export class AdminComponent {
     document.getElementById("all-bills")!.style.display = "none";
   }
 
+  /*------------------------------------View All Bills by Consumer Id--------------------------------------------*/
+
+  showBillByCidForm() {
+    document.getElementById("bill-by-cid-form")!.style.display = "block";
+  }
+
+  closeBillByCidForm() {
+    document.getElementById("bill-by-cid-form")!.style.display = "none";
+    this.closeBillByCidTable();
+  }
+
+  getBillByCid(data: any) {
+    if(data.email.trim() === "") {
+      alert("Select a Email")
+    } else {
+      this.adminService.viewBillByCid(data.email).subscribe(bill => {
+      this.billsByCid = bill;
+    })
+    this.showBillByCidTable();
+    }
+  }
+
+  showBillByCidTable() {
+    document.getElementById("all-bills-by-cid")!.style.display = "block";
+  }
+
+   closeBillByCidTable() {
+    document.getElementById("all-bills-by-cid")!.style.display = "none";
+  }
+
+  /*------------------------------------View Bill By Month and Year--------------------------------------------*/
+  showBillByMnYForm() {
+    document.getElementById("bill-by-mny-form")!.style.display = "block";
+  }
+
+  closeBillByMnYForm() {
+    document.getElementById("bill-by-mny-form")!.style.display = "none";
+    this.closeBillByMnYTable();
+  }
+
+  getBillByMnY(data: any) {
+    if(data.month === "" || data.year == 0) {
+      alert("Enter All Values")
+    } else {
+      this.adminService.viewBillByMnY(data.month, data.year).subscribe(bill => {
+      this.billsByMnY = bill;
+    })
+    this.showBillByMnYTable();
+    }
+  }
+
+  showBillByMnYTable() {
+    document.getElementById("all-bills-by-mny")!.style.display = "block";
+  }
+
+  closeBillByMnYTable() {
+    document.getElementById("all-bills-by-mny")!.style.display = "none";
+  }
+
+  /*------------------------------------View Bill By City--------------------------------------------*/
+  showBillByCityForm() {
+    document.getElementById("bill-by-city")!.style.display = "block";
+  }
+
+  closeBillByCityForm() {
+    document.getElementById("bill-by-city")!.style.display = "none";
+    this.closeBillByCityTable();
+  }
+
+  getBillByCity(data: any) {
+    if(data.city === "") {
+      alert("Select a city")
+    } else {
+      this.adminService.viewBillByCity(data.city).subscribe(bill => {
+      this.billsByCity = bill;
+    })
+    this.showBillByCityTable();
+    }
+  }
+
+  showBillByCityTable() {
+    document.getElementById("all-bills-by-city")!.style.display = "block";
+  }
+
+  closeBillByCityTable() {
+    document.getElementById("all-bills-by-city")!.style.display = "none";
+  }
+
+  /*------------------------------------View Bill By Area--------------------------------------------*/
+  showBillByAreaForm() {
+    document.getElementById("bill-by-area")!.style.display = "block";
+  }
+
+  closeBillByAreaForm() {
+    document.getElementById("bill-by-area")!.style.display = "none";
+    this.closeBillByAreaTable();
+  }
+
+  getBillByArea(data: any) {
+    if(data.area === "") {
+      alert("Select an area")
+    } else {
+      this.adminService.viewBillByArea(data.area).subscribe(bill => {
+      this.billsByArea = bill;
+    })
+    this.showBillByAreaTable();
+    }
+  }
+
+  showBillByAreaTable() {
+    document.getElementById("all-bills-by-area")!.style.display = "block";
+  }
+
+  closeBillByAreaTable() {
+    document.getElementById("all-bills-by-area")!.style.display = "none";
+  }
+
   /*------------------------------------View Area By City--------------------------------------------*/
   showAreaCityForm() {
     document.getElementById("area-by-city")!.style.display = "block";
@@ -196,7 +388,101 @@ export class AdminComponent {
           (          data: any) => console.log('success', data),
           (          error: HttpErrorResponse) => {
             if(error.status === 202)
-              alert(error.statusText + " - Modified Successfully");
+              alert("Modified Successfully!!!");
+            else
+              alert(error.statusText)
+          }
+
+        )
+    }
+  }
+
+  /*------------------------------------Modify City Area Name--------------------------------------------*/
+
+  showModifyCityAreaNameForm() {
+    document.getElementById("modify-city-area-name")!.style.display = "block";
+  }
+
+  closeModifyCityAreaNameForm() {
+    document.getElementById("modify-city-area-name")!.style.display = "none";
+  }
+
+  modifycityarea(data: {areaName: string, newCityName: string}) {
+    if(data.areaName === "") {
+      alert("Please select an Area");
+    } else if (data.newCityName.trim() === "") {
+      alert("Please Write the New City Name");
+    } else if (data.newCityName.trim() === data.areaName.trim()) {
+      alert("Old Name is same as the New Name")
+    }
+
+    else {
+        this.adminService.modifyCityAreaName(data).subscribe(
+          (          data: any) => console.log('success', data),
+          (          error: HttpErrorResponse) => {
+            if(error.status === 202)
+              alert("Modified Successfully!!!");
+            else
+              alert(error.statusText)
+          }
+
+        )
+    }
+  }
+
+  /*------------------------------------Modify City Name--------------------------------------------*/
+
+  showModifyCityNameForm() {
+    document.getElementById("modify-city-name")!.style.display = "block";
+  }
+
+  closeModifyCityNameForm() {
+    document.getElementById("modify-city-name")!.style.display = "none";
+  }
+
+  modifyCity(data: {cityName: string, newCityName: string}) {
+    if(data.cityName === "") {
+      alert("Please select an City");
+    } else if (data.newCityName.trim() === "") {
+      alert("Please Write the New City Name");
+    } else if (data.newCityName.trim() === data.cityName.trim()) {
+      alert("Old Name is same as the New Name")
+    }
+
+    else {
+        this.adminService.modifyCityName(data).subscribe(
+          (          data: any) => console.log('success', data),
+          (          error: HttpErrorResponse) => {
+            if(error.status === 202)
+              alert("Modified Successfully!!!");
+            else
+              alert(error.statusText)
+          }
+
+        )
+    }
+  }
+/*------------------------------------Modify Rate--------------------------------------------*/
+
+  showModifyRateForm() {
+    document.getElementById("modify-rate")!.style.display = "block";
+  }
+
+  closeModifyRateForm() {
+    document.getElementById("modify-rate")!.style.display = "none";
+  }
+
+  modifyrate(data: {consumerType: string, newRate: number}) {
+    if(data.consumerType === null) {
+      alert("Please select an Consumer Type");
+    } else if (data.newRate === null) {
+      alert("Please Write the New City Name");
+    } else {
+        this.adminService.modifyRate(data).subscribe(
+          (          data: any) => console.log('success', data),
+          (          error: HttpErrorResponse) => {
+            if(error.status === 202)
+              alert("Modified Successfully!!!");
             else
               alert(error.statusText)
           }
